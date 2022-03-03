0c3d32122493cc9484df2022b326cf8295697b36
Mårten Björkman <marten.bjorkman@apelkado.com>
2022-03-03 11:38:06 +0100

    Implemented function to save AT command return to a string and enabled debugging.

--- projects/temperature_sensor_project/at_command.c
+++ projects/temperature_sensor_project/at_command.c
@@ -1,17 +1,46 @@
 /**
  * @file        at_command.c
- * @author      Jonathan Bergqvist 
+ * @author      Jonathan Bergqvist, Mårten Björkman
  * @brief       Defines functions that can be use to communicate with an ESP8266 module via uart.
  * @version     1.0
  * @date        2022-02-23
  */
+
 #include "at_command.h"
-#include "usart.h"
-#include "gd32vf103.h"
-#include "lcd.h"
 
 uint8_t ok_end_sequence_matches = 0;
+uint8_t error_end_sequence_matches = 0;
 DATA_TRANSMIT_STATE transmit_state = READY_TO_SEND;
+
+/**
+ * @brief      Get last return and save it to a string for easier handling.
+ * @param[out] string String to put the return contents in.
+ * @param      string_len The maximum length of the string.
+ * @return     The length of the data that was stored in the string.
+ */
+void get_last_return_string(char *string, int string_len) {
+    int return_length = 0;
+    int buffer_ptr = 0;
+    int string_ptr = string_len - 1;
+    char buffer[RECIEVE_BUFFER_SIZE] = {0};
+    char current_char;
+
+    while((current_char = getChar()) != '\0')
+    {
+        buffer[buffer_ptr] = current_char;
+        buffer_ptr++;
+        return_length++;
+    }
+
+    if (return_length > string_len)
+        while(string_ptr >= 0)
+            string[string_ptr--] = buffer[buffer_ptr--];
+    else
+        strncpy(string, buffer, string_len);
+
+    return return_length;
+
+}
 
 /**
  * @brief       callback function for the uart data recieve callback. Its' purpose is to
@@ -34,6 +63,57 @@ void wifi_uart_data_recieved_callback(uint8_t recieved_data)
     else
     {
         ok_end_sequence_matches = 0;
+    }
+
+    // NOTE: Without handling of erroneous return, the at_send function will get stuck forever waiting for 'AT_DONE'.
+    if (recieved_data == AT_RECIEVE_ERROR[error_end_sequence_matches])
+    {
+        error_end_sequence_matches++;
+        if (error_end_sequence_matches == AT_RECIEVE_ERROR_LENGTH)
+        {
+            //transmit_state = AT_DONE; // Don't allow to continue
+            error_end_sequence_matches = 0;
+#ifdef DEBUG
+            char string_return[RECIEVE_BUFFER_SIZE + 1] = {'\0'};
+            char at_command[RECIEVE_BUFFER_SIZE + 1] = {'\0'};
+            char at_message[RECIEVE_BUFFER_SIZE + 1] = {'\0'};
+            get_last_return_string(string_return, RECIEVE_BUFFER_SIZE);
+            char debug_message[1024] = {'\0'};
+
+            int string_return_ptr = 0;
+            int sub_str_ptr = 0;
+            while(string_return_ptr < RECIEVE_BUFFER_SIZE) {
+                if (string_return[string_return_ptr] == '\r' && string_return[string_return_ptr + 1] == '\n') {
+                    string_return_ptr += 2;
+                    break;
+                } else {
+                    at_command[sub_str_ptr++] = string_return[string_return_ptr++];
+                }
+            }
+
+            sub_str_ptr = 0;
+            while(string_return_ptr < RECIEVE_BUFFER_SIZE) {
+                if (string_return[string_return_ptr] == '\r' && string_return[string_return_ptr + 1] == '\n') {
+                    string_return_ptr += 2;
+                    if (sub_str_ptr > 0)
+                        at_message[sub_str_ptr++] = ';';
+                } else {
+                    at_message[sub_str_ptr++] = string_return[string_return_ptr++];
+                }
+            }
+
+            strncpy(debug_message, "AT command (", RECIEVE_BUFFER_SIZE);
+            strncat(debug_message, at_command, RECIEVE_BUFFER_SIZE);
+            strncat(debug_message, ") ", RECIEVE_BUFFER_SIZE);
+            strncat(debug_message, "returned an error: ", RECIEVE_BUFFER_SIZE);
+            strncat(debug_message, at_message, RECIEVE_BUFFER_SIZE);
+            debug_error_message_custom(debug_message);
+#endif
+        }
+    }
+    else
+    {
+        error_end_sequence_matches = 0;
     }
 }
 
@@ -99,6 +179,11 @@ int8_t _is_command_length_within_limits(char * at_command)
  */
 int at_send(char *at_command, uint8_t response_falg)
 {
+    // NOTE: Must flush buffer to put pointer in correct place in case previous caller to at_send() didn't read
+    // the return. Only now we really know that if reading (with getChar()) after at_send(), all data will come
+    // from the LATEST return.
+    while(getChar() != '\0');
+
     if(!_is_command_length_within_limits(at_command))
     {
         return -1;
@@ -114,6 +199,9 @@ int at_send(char *at_command, uint8_t response_falg)
             DATA_TRANSMIT_STATE current_state = _get_transmit_state();
             if (current_state == WAITING)
             {
+                // TODO: A timeout is REALLY needed here. Sometimes commands are not understood and then there will
+                //  never be any OK or ERROR sent. Now execution stops here on every other reboot because the device
+                //  randomly resets and during this reset, commands won't be understood.
                 u0_TX_Queue();
                 continue;
             }
